edfd03a2b68e94c3b126d1dba18b1624adf39868
Stepan Ruzicka <stepan.ruzicka@bluewolfgroup.com>
2018-04-09 13:13:31 +0200

    Scripts updated

--- bin/deploy_vlocity_metadata.py
+++ bin/deploy_vlocity_metadata.py
@@ -23,6 +23,7 @@ this = sys.modules[__name__]
 DEBUG = False
 IGNORE_ERRORS = False
 DEBUG_LEVEL = 1
+OPTIMIZATION = True
 
 ORGS_JSON_PATH = 'config'
 ORGS_JSON_FILENAME = '.orgs.json'
@@ -122,47 +123,13 @@ def get_remote_org_configuration(org_json_file_path):
          raise SystemExit(ORGS_JSON_FILENAME + ' not found!')
    return remotes
 
-def deploy_vlocity_metadata(row, target_env, remotes, deployed_components):
-   skipped = False
-   deploy_cmd = ''
-   result = ''
-      
-   remote = get_remote(target_env, remotes)
-   if('Backlog_Item__r.Name' not in row):
-      raise SystemExit("'Backlog_Item__r.Name' key is missing in input data")
-   elif('Retrieval_Status' not in row):
-      raise SystemExit("'Retrieval_Status' key is missing in input data")
-   elif('Job_File_Path' not in row):
-      raise SystemExit("'Job_File_Path' key is missing in input data")
-
-   item = row['Backlog_Item__r.Name']
-               
-   if(row['Retrieval_Status'] == 'Success' or this.IGNORE_ERRORS):
-      if('Job_File_Path' in row and os.path.exists(row['Job_File_Path'])):
-         deploy_cmd = 'vlocity packDeploy -job="' + row['Job_File_Path'] + '" -sf.username="'  + remote['username'] + '" -sf.password="' + remote['password'] + '" -sf.loginUrl="' + remote['serverUrl'] + '"'
-         print_info('Running command: ' + color_string(deploy_cmd, Color.BLUE))
-         #result = commands.getoutput(deploy_cmd)
-         try:
-            result = subprocess.check_output(deploy_cmd, shell=True)
-         except subprocess.CalledProcessError as e:
-            if(not INGORE_ERRORS)
-               raise RuntimeError("Deployment failed (Command: '{}' returned error (code {}). If you want to ignore errors during the deployment you can run it with --ignore-errors parameter. Please, also use -d parameter for more details".format(e.cmd, e.returncode))
-            result = e.output
-
-         #print(commands.getoutput(deploy_cmd))
-      else:
-         print_info(color_string('Package path not specified or doesn\'t exist!', Color.RED))
-   else:
-      print_info('Skipping ' + color_string(item, Color.BLUE) + ' as it wasn\'t successfully retrieved (' + row['Retrieval_Details'] + ')')
-      skipped = True
-
-   if(this.DEBUG and this.DEBUG_LEVEL == 2):
-      print_info(result)      
-
+def remove_formatting(result):
    # remove formatting from the result
    ansi_escape = re.compile(r'[\s]?\x1b\[[0-?]*[ -/]*[@-~]')
    result = ansi_escape.sub('', result)
+   return result
 
+def process_result(result, item, deployed_components, row):
    # transform result to dictionary
    result_dict = {}
    for line in result.splitlines():
@@ -173,19 +140,11 @@ def deploy_vlocity_metadata(row, target_env, remotes, deployed_components):
          else:
             result_dict[result_array[0].strip()].append(result_array[1].strip())
       else:
-         result_dict[result_array[0].strip()] = ''
-  
-   #print(result_dict) 
-
-   # initialize deployment_components with values from retrieval
-   deployed_components[row['Backlog_Item__r.Name']] = row.copy()
+         result_dict[result_array[0].strip()] = ''  
+   #print(result_dict)
 
    # default values
-   deployment_status = ''
-   if(skipped):
-      deployment_status = 'Skipped'
-   else:
-      deployment_status = 'Success'
+   deployment_status = 'Success'
 
    deployment_details = row['Job_File_Path']
    deployment_error_message = ''
@@ -193,7 +152,7 @@ def deploy_vlocity_metadata(row, target_env, remotes, deployed_components):
    # check errors
    if(get_error(result_dict)):
       deployment_status = 'Failed'
-      deployment_error_message = 'Errors for item: ' + row['Backlog_Item__r.Name'] + ' (' + deployment_detials + ')'
+      deployment_error_message = 'Errors for item: ' + item + ' (' + deployment_detials + ')'
       deployment_error_message = deployment_error_message + '\n' + get_error_message(result_dict)
       if(IGNORE_ERRORS):
          print_info(color_string(deployment_error_message, Color.RED))
@@ -201,22 +160,80 @@ def deploy_vlocity_metadata(row, target_env, remotes, deployed_components):
          raise SystemExit(color_string(deployment_error_message, Color.RED))
  
    # if successfully deployed add component to deployed_components dictionary
-   if(row['Backlog_Item__r.Name'] not in deployed_components):
+   if(item not in deployed_components):
       # add the original csv row to the result
       deployed_components[row['Backlog_Item__r.Name']] = row.copy()
       
    # update deployment status
-   deployed_components[row['Backlog_Item__r.Name']]['Deployment_Status'] = deployment_status
-   deployed_components[row['Backlog_Item__r.Name']]['Deployment_Error_Message'] = deployment_error_message
-   deployed_components[row['Backlog_Item__r.Name']]['Deployment_Details'] = deployment_details
+   deployed_components[item]['Deployment_Status'] = deployment_status
+   deployed_components[item]['Deployment_Error_Message'] = deployment_error_message
+   deployed_components[item]['Deployment_Details'] = deployment_details
 
    if(deployment_status == 'Success'):
-      deployed_components[row['Backlog_Item__r.Name']]['Components'] = get_successfully_deployed_components(result_dict)
-      print_info('Successfully deployed')
+      deployed_components[item]['Components'] = get_successfully_deployed_components(result_dict)
+      return 'Successfully deployed'
    else:
-      print_info(deployment_status)
+      return deployment_status
    
    #subprocess.check_output(retrieve_cmd, shell=True)
+
+def update_deployed_components_status(item, deployed_components, status):
+   deployed_components[item]['Deployment_Status'] = status
+   return True
+
+def copy_result(item, deployed_components, same_folder_item):
+   deployed_components[item] = dict(deployed_components[same_folder_item])
+
+def deploy_vlocity_metadata(row, target_env, remotes, deployed_components, optimization_dict):
+   skipped = False
+   deploy_cmd = ''
+   result = ''
+      
+   remote = get_remote(target_env, remotes)
+
+   # check whether necessary columns are present
+   if('Backlog_Item__r.Name' not in row):
+      raise SystemExit("'Backlog_Item__r.Name' key is missing in input data")
+   elif('Job_File_Path' not in row):
+      raise SystemExit("'Job_File_Path' key is missing in input data")
+
+   item = row['Backlog_Item__r.Name']
+
+   # initialize deployment_components with values from retrieval
+   deployed_components[row['Backlog_Item__r.Name']] = row.copy()
+
+   if('Retrieval_Status' not in row or row['Retrieval_Status'] == 'Success' or this.IGNORE_ERRORS):
+      if('Job_File_Path' in row and os.path.exists(row['Job_File_Path'])):
+         # if optimization is turned on then don't deploy items with the same output folder
+         if(not OPTIMIZATION or ('Output_Folder' in row and row['Output_Folder'] not in optimization_dict)):
+            deploy_cmd = 'vlocity packGetDiffsAndDeploy -job="' + row['Job_File_Path'] + '" -sf.username="'  + remote['username'] + '" -sf.password="' + remote['password'] + '" -sf.loginUrl="' + remote['serverUrl'] + '"'
+            print_info('Running command: ' + color_string(deploy_cmd, Color.BLUE))
+            #result = commands.getoutput(deploy_cmd)
+            try:
+               result = subprocess.check_output(deploy_cmd, shell=True)
+            except subprocess.CalledProcessError as e:
+               if not IGNORE_ERRORS:
+                  raise RuntimeError("Deployment failed (Command: '{}' returned error (code {}). If you want to ignore errors during the deployment you can run it with --ignore-errors parameter. Please, also use -d parameter for more details".format(e.cmd, e.returncode))
+               result = e.output
+            
+            # print info original formatting included
+            if(this.DEBUG and this.DEBUG_LEVEL == 2):
+               print_info(result)
+ 
+            result = remove_formatting(result)
+            
+            deployment_status = process_result(result, item, deployed_components, row)
+            print_info(color_string(deployment_status, Color.BLUE))
+            #print(commands.getoutput(deploy_cmd))
+         else:
+            deployment_status = copy_result(item, deployed_components, deployed_components[row['Output_Folder']])
+      else:
+         print_info(color_string('Package path not specified or doesn\'t exist!', Color.RED))
+   else:
+      print_info('Skipping ' + color_string(item, Color.BLUE) + ' as it wasn\'t successfully retrieved (' + row['Retrieval_Details'] + ')')
+      update_deployed_components_status(item, deployed_components, 'Skipped')
+      # print deployment status
+      print_info(color_string('Skipped', Color.Blue))
 
 def get_error(result):
    if "Errors" in result:
@@ -271,6 +288,28 @@ def write_csv_output(deployed_components):
          print_info('For item ' + color_string(item, Color.BLUE) + ' deployed components are:')
          for component in deployed_components[item]['Components']:         
             print_info('\t' + component)
+
+def get_vlocity_yaml_file(vlocity_deploy_template_path, package_path):
+   vlocity_yaml = ''
+   template_file = Path(vlocity_deploy_template_path)
+   if(not template_file.is_file()):
+      raise SystemExit('Vlocity deploy template file: ' + vlocity_deploy_template_path + ' not found!')
+   else:
+      print_info('Using vlocity yaml template: ' + color_string(vlocity_deploy_template_path, Color.BLUE))
+      # replace all keywords
+      with open(vlocity_deploy_template_path, 'r') as vlocity_template:
+         vlocity_yaml = vlocity_template.read().replace('%feature%', package_path).replace('%', '')
+   return vlocity_yaml
+
+def write_temporary_yaml_file(vlocity_yaml_file_path, vlocity_yaml_file_string):
+   print_info('Creating temporary yaml file: ' + color_string(vlocity_yaml_file_path, Color.BLUE))
+   folder = os.path.dirname(vlocity_yaml_file_path)
+   if not os.path.exists(folder):
+      os.makedirs(folder)
+
+   with open(vlocity_yaml_file_path, "w") as vlocity_yaml_file:
+      vlocity_yaml_file.write("%s" % vlocity_yaml_file_string)
+
 def main():
    parser = argparse.ArgumentParser(description='Exports vlocity components from a source SF environment.\n' +
                                                 'Example:\n' +
@@ -303,6 +342,8 @@ def main():
    orgs_json_file_path = Path(ORGS_JSON_PATH + '/' + ORGS_JSON_FILENAME)
    remotes = get_remote_org_configuration(orgs_json_file_path)
 
+   vlocity_template_path = SCRIPT_FOLDER_PATH + '/../' + CONFIG + '/' + TEMPLATES_FOLDER + '/' + VLOCITY_YAML_TEMPLATE
+
    # TODO check whether is force-dev-tool installed
    #if(platform.system() != 'Windows' and not is_tool("force-dev-tool")):
    if(platform.system() != 'Windows' and not is_tool("force-dev-tool")):
@@ -315,33 +356,77 @@ def main():
    # loop through input (stdin)
    counter = 0
    #csv_reader = csv.DictReader(fileinput.input(mode='rb'), delimiter=',')
-   csv_reader = csv.DictReader(iter(sys.stdin.readline, ''))
 
-   deployed_components = {}
-   for row in csv_reader:
-      counter = counter + 1
-      
-      # print new line
-      if(counter > 1):
-         print_info('\n')
-      output_folder = ''
-
-      print_info(color_string('Component #' + str(counter) + ' (' + row['Backlog_Item__r.Name'] + ')', Color.GREEN))
-      #print(row)
-
-      # get_vlocity_yaml_file
-      #vlocity_template_path = SCRIPT_FOLDER_PATH + '/' + CONFIG + '/' + TEMPLATES_FOLDER + '/' + VLOCITY_YAML_TEMPLATE
-      #vlocity_yaml_file_string = get_vlocity_yaml_file(vlocity_template_path, other_conditions, object_map_by_vlocity_type, row, output_folder)
-      
-      # write temporary yaml file
-      #vlocity_yaml_file_path = TEMP + '/' + row['Backlog_Item__r.Name'] + '.yaml'
-      #write_temporary_yaml_file(vlocity_yaml_file_path, vlocity_yaml_file_string)
-      
+   input_list = list(iter(sys.stdin.readline, ''))
+   # comma separated values?
+   comma_separated_values = input_list[0].count(",")
+   
+   deployed_components = {}  
+ 
+   # process input as csv
+   if(comma_separated_values):
+      # CSV on input
       for target_env in environments:
-         # retrieve vlocity metadata for the current row
-         deploy_vlocity_metadata(row, target_env, remotes, deployed_components)
+         csv_reader = csv.DictReader(input_list)
+         #print(target_env)
+         #print(csv_reader)
+         deployed_components = {}
+         optimization_dict = {}
+    
+         counter = 0
+         for row in csv_reader:
+            counter = counter + 1
+       
+            # print new line
+            if(counter > 1):
+               print_info('\n')
+            output_folder = ''
+
+            print_info(color_string('Deploy - Component #' + str(counter) + ' (' + row['Backlog_Item__r.Name'] + ')', Color.GREEN))
+            #print(row)
+
+            # get_vlocity_yaml_file
+            #vlocity_template_path = SCRIPT_FOLDER_PATH + '/' + CONFIG + '/' + TEMPLATES_FOLDER + '/' + VLOCITY_YAML_TEMPLATE
+            #vlocity_yaml_file_string = get_vlocity_yaml_file(vlocity_template_path, other_conditions, object_map_by_vlocity_type, row, output_folder)
       
-   write_csv_output(deployed_components)
+            # write temporary yaml file
+            #vlocity_yaml_file_path = TEMP + '/' + row['Backlog_Item__r.Name'] + '.yaml'
+            #write_temporary_yaml_file(vlocity_yaml_file_path, vlocity_yaml_file_string)
+      
+            # retrieve vlocity metadata for the current row
+            deploy_vlocity_metadata(row, target_env, remotes, deployed_components, optimization_dict)
+   else:
+      for target_env in environments:
+         #process input as list of package paths
+         # we need to create yaml file for each item
+         deployed_components = {}
+         optimization_dict = {}
+         counter = 0 
+         for package_path in input_list:
+            counter = counter + 1
+            # print new line
+            if(counter > 1):
+               print_info('\n')
+
+            package_name = os.path.basename(package_path.rstrip())
+            temporary_yaml_file_path = TEMP + '/' + package_name.rstrip() + '.yaml'
+            package_folder = Path(package_path.rstrip())
+                        
+            if(not package_folder.is_dir()):
+               raise SystemExit('Package folder: ' + package_path.rstrip() + ' not found!')
+            else:
+               write_temporary_yaml_file(temporary_yaml_file_path, get_vlocity_yaml_file(vlocity_template_path, package_path))
+            
+            row = {}
+            row['Backlog_Item__r.Name'] = package_name
+            row['Job_File_Path'] = temporary_yaml_file_path
+            row['Output_Folder'] = package_folder
+
+            print_info(color_string('Deployment to ' + target_env + ' - Component #' + str(counter) + ' (' + row['Backlog_Item__r.Name'] + ')', Color.GREEN))
+
+            deploy_vlocity_metadata(row, target_env, remotes, deployed_components, optimization_dict)
+
+         write_csv_output(deployed_components)
  
 if __name__ == "__main__":
    main()
