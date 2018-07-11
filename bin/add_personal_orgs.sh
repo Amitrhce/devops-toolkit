42da7ad92096c12c5aff0ab9eb46dbc0c8ccd6a5
Stepan Ruzicka <stepan.ruzicka@bluewolfgroup.com>
2018-07-11 16:51:31 +0100

    add_personal_orgs.sh added

---
+++ bin/add_personal_orgs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SANDBOX_URL='https://test.salesforce.com'
+
+force-dev-tool remote add jpopr sravaniv@in.ibm.com.three.jpopr Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add jkova sravaniv@in.ibm.com.three.jkova Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add hjose sravaniv@in.ibm.com.three.hjose Sarusravs1 $SANDBOX_URL
+force-dev-tool remote add anyam sravaniv@in.ibm.com.three.anyam Sarusravs1 $SANDBOX_URL
+force-dev-tool remote add jgree sravaniv@in.ibm.com.three.jgree Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add mprak sravaniv@in.ibm.com.three.mprak Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add agoya sravaniv@in.ibm.com.three.agoya Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add madam sravaniv@in.ibm.com.three.madam Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add mrade sravaniv@in.ibm.com.three.mrade Sarusravs\$1 $SANDBOX_URL
+force-dev-tool remote add vinechha sravaniv@in.ibm.com.three.vinechha Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add pbhat sravaniv@in.ibm.com.three.pbhat Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add rkumar sravaniv@in.ibm.com.three.rkumar Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add skund sravaniv@in.ibm.com.three.skund Sarusravs2 $SANDBOX_URL
+force-dev-tool remote add rsank sravaniv@in.ibm.com.three.rsank Sarusravs2 $SANDBOX_URL
